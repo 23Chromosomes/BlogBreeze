0c063e12539534ffe2b23f2440376f1534b6df6c
hendrik.visser <hendrikpfaff2@hotmail.com>
2021-06-12 00:17:01 +0200

    V9.1
    Added image I forgot

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -21,11 +21,11 @@
 @auth
 <x-app-layout>
 
-    <div class="py-12">
+    <div class="py-12 flex justify-center">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    RANDOM ONZIN (Authenticated)
+                    <img src="{{url('/media/gadgetimage.jpg')}}" alt="Image" style="max-height: 80vh;" class="mx-0"/>
                 </div>
             </div>
         </div>
